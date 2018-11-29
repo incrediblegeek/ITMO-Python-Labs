e4324433fa5d377a65a11189839d149a22ac0bee
incrediblegeek <incrediblegeek@users.noreply.github.com>
2018-11-29 18:57:06 +0300

    Update draw_cell_list()

--- homework03/life.py
+++ homework03/life.py
@@ -25,30 +25,24 @@ class GameOfLife:
     def draw_grid(self):
         """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                    (x, 0), (x, self.height))
+            pygame.draw.line(self.screen, pygame.Color('black'), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color('black'),
-                    (0, y), (self.width, y))
+            pygame.draw.line(self.screen, pygame.Color('black'), (0, y), (self.width, y))
 
     def run(self):
         """ Запустить игру """
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption('Game of Life')
-        self.screen.fill(pygame.Color('white'))
-
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
-
+        self.screen.fill(pygame.Color('Black'))
+        self.clist = self.cell_list()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     running = False
             self.draw_grid()
-
-            # Отрисовка списка клеток
+            self.draw_cell_list(self.clist)
             # Выполнение одного шага игры (обновление состояния ячеек)
             # PUT YOUR CODE HERE
 
@@ -64,11 +58,17 @@ class GameOfLife:
         return self.clist
 
     def draw_cell_list(self, clist):
-        """ Отображение списка клеток
-
-        :param rects: Список клеток для отрисовки, представленный в виде матрицы
-        """
-        pass
+        """ Отображение списка клеток """
+        for h in range(self.cell_height):
+            for w in range(self.cell_width):
+                x = w * self.cell_size
+                y = h * self.cell_size
+                a = self.cell_size
+                b = self.cell_size
+                if clist[h][w] == 0:
+                    pygame.draw.rect(self.screen, pygame.Color('Black'), (x, y, a, b))
+                else:
+                    pygame.draw.rect(self.screen, pygame.Color('Red'), (x, y, a, b))
 
     def get_neighbours(self, cell):
         """ Вернуть список соседей для указанной ячейки
@@ -92,3 +92,7 @@ class GameOfLife:
         new_clist = []
         # PUT YOUR CODE HERE
         return self.clist
+
+if __name__ == '__main__':
+    game = GameOfLife(640, 480, 10)
+    game.run()
